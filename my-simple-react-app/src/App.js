1dd54513b00e23882e6613b15bd909f5c2402c8f
DhruvP <dhruvpareek883@gmail.com>
2024-05-13 13:21:16 -0700

    Updated blockchain link

--- my-simple-react-app/src/App.js
+++ my-simple-react-app/src/App.js
@@ -12,7 +12,7 @@ function Home() {
                 <li><a href="http://dartstracker301.xyz" target="_blank" rel="noopener noreferrer">Darts Tracker</a></li>
                 <li><a href="https://github.com/DhruvPareek/HIllTalkV2" target="_blank" rel="noopener noreferrer">Hilltalk</a></li>
                 <li><a href="https://bold-doll-6ca.notion.site/cb6726e483d14fedb2ad2632f65e1407?v=1c8cf307c77e4ad4854e4a4cd9e6abc6" target="_blank" rel="noopener noreferrer">Blockchain Learning Resource</a></li>
-                <li><a href="https://youtu.be/hN5xxrnfPlI?si=evBJEuM8DsxV7D7V" target="_blank" rel="noopener noreferrer">Blockchain@UCLA presentation</a></li>
+                <li><a href="https://youtube.com/playlist?list=PLsmKXxXxQhJSZUidAgoepyXiL37bdPtx1&si=LRrmYGGCmpFVV_Cm" target="_blank" rel="noopener noreferrer">Blockchain@UCLA presentations</a></li>
                 <li><a href="https://www.youtube.com/@dpak576" target="_blank" rel="noopener noreferrer">Videos</a></li>
             </ul>
         </div>
