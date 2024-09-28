ee53359d78bf0fbd42e8f66d4cc977eb1afd820f
DhruvP <dhruvpareek883@gmail.com>
2024-09-28 11:46:45 -0700

    Added Safe transactions

--- my-simple-react-app/src/App.js
+++ my-simple-react-app/src/App.js
@@ -9,6 +9,7 @@ function Home() {
             <div className='content'>
                 <h2>Random funky stuff:</h2>
                 <ul>
+                    <li><a href="https://safe-transactions-frontend.vercel.app/" target="_blank" rel="noopener noreferrer">Safe Transactions</a></li>
                     <li><a href="http://dartstracker301.xyz" target="_blank" rel="noopener noreferrer">Darts Tracker</a></li>
                     <li><a href="https://github.com/DhruvPareek/HIllTalkV2" target="_blank" rel="noopener noreferrer">Hilltalk</a></li>
                     <li><a href="https://bold-doll-6ca.notion.site/cb6726e483d14fedb2ad2632f65e1407?v=1c8cf307c77e4ad4854e4a4cd9e6abc6" target="_blank" rel="noopener noreferrer">Blockchain Learning Resource</a></li>
--- my-simple-react-app/src/index.js
+++ my-simple-react-app/src/index.js
@@ -4,3 +4,5 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
+//ALWAYS DOWNGRADE TO WITH COMMAND: nvm use v16.20.2 before running npm start
