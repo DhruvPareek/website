9f4aaaa5ec4a52b9ba8509296b3cf49ae4f3416d
DhruvP <dhruvpareek883@gmail.com>
2025-06-22 12:53:05 -0700

    Fixed build

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import '../Styling/App.css';
 import ScaleEssay from './ScaleEssay.tsx';
